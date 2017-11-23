70f567676f193f706a632d78f30a294600c6ee9c
Serena <serena.f.yu@gmail.com>
2017-11-23 13:55:13 +1100

    preliminarily working

--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -7,28 +7,28 @@
 using namespace sf;
 using namespace std;
 
-const byte step = 10;		  // size of each brick
-const byte padding = step;
-const int baseMoveInterval = 500;
-Time keyInterval, moveInterval;
+const int step = 10;		  // size of each brick
+const int padding = step;
+const int baseMoveInterval = 400;
+Time keyInterval, moveInterval, rotateInterval;
 bool rotating = 0;
-unordered_map<int_fast32_t, byte> pow2;
+unordered_map<int_fast32_t, int> pow2;
 
 
-Clock moveClock, keyClock;
+Clock moveClock, keyClock, rotateClock;
 unordered_set<RectangleShape*> rects;
-RectangleShape* mainRectGrid[25][56];
-int_fast32_t globalGrid[56];
+RectangleShape* mainRectGrid[28][57];
+int_fast32_t globalGrid[57];
 unsigned long score = 0;
 Text txtScore;
 
 //game control
-byte gameStatus = 0;   //waiting to start -- 0, game on -- 1, game over -- 2 					   		
+int gameStatus = 0;   //waiting to start -- 0, game on -- 1, game over -- 2 					   		
 Point refreshPoint(12, 6);
 Point previewPoint(20, 1);
 Block preview = Block();
 Block running = Block();
-byte direction = 0;
+int direction = 0;
 
 //random
 random_device rd;
@@ -38,13 +38,14 @@ uniform_int_distribution<int> distColor(50, 205);
 
 int WinMain()
 {
-	RenderWindow window(VideoMode(270, 570), "Tetris");
+	RenderWindow window(VideoMode(280, 570), "Tetris");
 	memset(mainRectGrid, 0, sizeof(mainRectGrid));
-	keyInterval = Time(milliseconds(100));
+	keyInterval = Time(milliseconds(70));
 	moveInterval = Time(milliseconds(baseMoveInterval));
+	rotateInterval = Time(milliseconds(baseMoveInterval));
 
 	//generate powers of 2
-	for (byte i = 0; i < 25; i++)
+	for (int i = 0; i < 25; i++)
 		pow2[1 << i] = i;
 
 	//Score
@@ -56,9 +57,9 @@ int WinMain()
 	txtScore.setFillColor(Color::Black);
 
 	//Border
-	RectangleShape border(Vector2f(250, 500));
+	RectangleShape border(Vector2f(260, 500));
 	border.setFillColor(Color::Transparent);
-	border.setPosition(Vector2f(padding, 59));
+	border.setPosition(Vector2f(padding, 60));
 	border.setOutlineColor(Color::Black);
 	border.setOutlineThickness(2.0);
 
@@ -90,8 +91,8 @@ int WinMain()
 				moveClock.restart();
 			}
 			//forced move
-			if (direction && keyClock.getElapsedTime() > keyInterval)
-			{
+			if (direction && keyClock.getElapsedTime() > keyInterval) {
+
 				if (!running.move(direction))			//if collides
 				{
 					eliminate();
@@ -101,10 +102,10 @@ int WinMain()
 				}
 				keyClock.restart();
 			}
-			if (rotating && keyClock.getElapsedTime() > keyInterval)
+			if (rotating && rotateClock.getElapsedTime() > rotateInterval)
 			{
 				running.rotate();
-				keyClock.restart();
+				rotateClock.restart();
 			}
 		}
 		window.clear(Color::White);
@@ -156,6 +157,7 @@ void procKeyPress(const Event& event)
 			gameStatus = 1;
 			init();
 		}
+		break;
 	}
 }
 
@@ -178,6 +180,7 @@ void procKeyUp(const Event& event)
 	case Keyboard::Up:
 		if (rotating)
 			rotating = 0;
+		break;
 	}
 }
 
@@ -199,11 +202,11 @@ void init()
 void eliminate()
 {
 	running.projection();
-	byte rowsEliminated = 0;
+	int rowsEliminated = 0;
 	bool found = 0;
-	byte first = 0, last = 0;
+	int first = 0, last = 0;
 	int32_t fullRow = (1 << 25) - 1;
-	for (byte i = 55; i >= 12 && globalGrid[i]; i--)
+	for (int i = 56; i >= 12 && globalGrid[i]; i--)
 	{
 		if (!first && globalGrid[i] == fullRow)
 			first = i;
@@ -217,7 +220,7 @@ void eliminate()
 	while (found)
 	{
 		found = 0;
-		for (byte i = first; i >= last; i--)
+		for (int i = first; i >= last; i--)
 			if (globalGrid[i] == fullRow)
 			{
 				found = 1;
@@ -230,14 +233,14 @@ void eliminate()
 					mainRectGrid[x][i] = NULL;
 				}
 			}
-		for (byte i = first; i >= last; i--)
+		for (int i = first; i >= last; i--)
 		{
 			if (globalGrid[i] == 0)
 			{
-				byte j = i;
+				int j = i;
 				while (globalGrid[j] == 0)
 					j--;
-				while (j < 56 && (globalGrid[j] & globalGrid[j - 1]) == 0)
+				while (j < 57 && (globalGrid[j] & globalGrid[j - 1]) == 0)
 				{
 					globalGrid[j - 1] = globalGrid[j] & globalGrid[j - 1];
 					globalGrid[j] = 0;
@@ -245,7 +248,7 @@ void eliminate()
 					found = 1;
 					for (int x = 0; x < 25; x++)
 					{
-						mainRectGrid[x][j]->setPosition(Vector2f(x, j - 1));
+						mainRectGrid[x][j]->setPosition(Vector2f(x * 10.0f, (j - 1) * 10.0f));
 						mainRectGrid[x][j - 1] = mainRectGrid[x][j];
 						mainRectGrid[x][j] = NULL;
 					}
@@ -264,10 +267,10 @@ void gameover()
 	gameStatus = 3;
 }
 
-RectangleShape* drawSquare(byte x, byte y, Color color)
+RectangleShape* drawSquare(int x, int y, Color color)
 {
 	RectangleShape* sq = new RectangleShape(Vector2f(step, step));
-	sq->setPosition(x * 10.0f + padding, y * 10.0f);
+	sq->setPosition(x * 10.0f, y * 10.0f);
 	sq->setFillColor(color);
 	sq->setOutlineColor(Color::Black);
 	sq->setOutlineThickness(1.0);
@@ -276,7 +279,7 @@ RectangleShape* drawSquare(byte x, byte y, Color color)
 
 // Class functions
 
-Point::Point(byte x0, byte y0)
+Point::Point(int x0, int y0)
 {
 	x = x0;
 	y = y0;
@@ -290,9 +293,8 @@ Point::Point(const Point& other)
 
 void Block::draw()
 {
-	int_fast32_t shiftLine;
-	for (int y = 0; y < 4; y++)
-		for (int x = 0; x < 4; x++)
+	for (int y = 0; y < 5; y++)
+		for (int x = 0; x < 5; x++)
 			if (grid[y] & (1 << x))
 			{
 				RectangleShape* newRect = drawSquare(x + position.x, y + position.y, foreColor);
@@ -303,38 +305,34 @@ void Block::draw()
 
 void Block::clearGraphic()
 {
-	int_fast32_t shiftLine;
-	for (int y = 0; y < 4; y++)
-	{
-		shiftLine = (int_fast32_t)grid[y] << position.x;
-		for (int x = 0; x < 4; x++)
+	for (int y = 0; y < 5; y++)
+		for (int x = 0; x < 5; x++)
 			if (grid[y] & (1 << x) && mainRectGrid[x + position.x][y + position.y])
 			{
 				rects.erase(mainRectGrid[x + position.x][y + position.y]);
 				delete mainRectGrid[x + position.x][y + position.y];
 				mainRectGrid[x + position.x][y + position.y] = NULL;
 			}
-	}
 }
 
 void Block::projection()
 {
 	// Project the local grid to the global grid
 	int_fast32_t shiftLine;
-	for (int y = 0; y < 4; y++)
+	for (int y = 0; y < 5; y++)
 	{
 		shiftLine = (int_fast32_t)grid[y] << position.x;
 		globalGrid[y + position.y] |= shiftLine;
 	}
 }
 
-bool Block::collision(Point position, char datagrid[4])
+bool Block::collision(Point position, char datagrid[5])
 {
-	for (int i = 3; i >= 0; i--)
-		if (datagrid[i] && position.y + i >= 55)
+	for (int i = 4; i >= 0; i--)
+		if (datagrid[i] && position.y + i >= 56)
 			return 1;
 	int_fast32_t shiftLine;
-	for (int y = 0; y < 4; y++)
+	for (int y = 0; y < 5; y++)
 	{
 		shiftLine = (int_fast32_t)datagrid[y] << position.x;
 		if (shiftLine & globalGrid[y + position.y])
@@ -343,7 +341,7 @@ bool Block::collision(Point position, char datagrid[4])
 	return 0;
 }
 
-void Block::set(sf::Color fColor, byte tpe, const Point& pos)
+void Block::set(sf::Color fColor, int tpe, const Point& pos)
 {
 	foreColor = fColor;
 	position = Point(pos);
@@ -353,23 +351,24 @@ void Block::set(sf::Color fColor, byte tpe, const Point& pos)
 	switch (type)
 	{
 	case 0:		//bar
-		for (int y = 0; y < 4; y++)
-			grid[y] = 2;
+		for (int y = 1; y < 5; y++)
+			grid[y] = 0b100;
 		break;
 	case 1:		//square
-		grid[1] = grid[2] = 6;
+		grid[2] = grid[3] = 0b1100;
 		break;
 	case 2:		// L
-		grid[0] = grid[1] = 2;
-		grid[2] = 6;
+		grid[1] = grid[2] = 0b100;
+		grid[3] = 0b1100;
 		break;
 	case 3:		// reverse L
-		grid[0] = grid[1] = 2;
-		grid[2] = 3;
+		grid[1] = grid[2] = 0b100;
+		grid[3] = 0b110;
 		break;
 	case 4:		//S
-		grid[0] = grid[2] = 2;
-		grid[1] = 6;
+		grid[1] = 0b100;
+		grid[2] = 0b1100;
+		grid[3] = 0b1000;
 		break;
 	}
 	if (collision(position, grid))
@@ -377,34 +376,44 @@ void Block::set(sf::Color fColor, byte tpe, const Point& pos)
 		gameover();
 		return;
 	}
+	findBorder(&left, &right, &bot, grid);
 	draw();
 }
 
-bool Block::rotate()
+void Block::rotate()
 {
-	char newGrid[4] = { 0,0,0,0 };
-	for (byte y = 0; y < 4; y++)
-		for (byte x = 0; x < 4; x++)
+	if (type == 1)
+		return;
+	char newGrid[5] = { 0,0,0,0,0 };
+	for (int y = 0; y < 5; y++)
+		for (int x = 0; x < 5; x++)
 		{
-			byte newX = (position.x + 1) - (y - (position.y + 1));		//pivot is the point (1,1) in grid
-			byte newY = position.y + 1 + (x - (position.x + 1));
-			if (position.x + newX < 0 || position.x + newX >24 || position.y + newY < 0 || position.y + newY >55)
-				return 0;
-			newGrid[newY] |= (grid[y] & 1 << x) << newX;
+			if (grid[y] & (1 << x))
+			{
+				int newX = 4 - y;		//pivot is the point (2,2) in grid
+				int newY = x;
+				newGrid[newY] |= 1 << newX;
+			}
 		}
+	int nleft, nright, nbot;
+	findBorder(&nleft, &nright, &nbot, newGrid);
+	if (position.x + nleft < 0 || position.x + nright > 26 || position.y + nbot > 56)
+		return;
 	if (collision(position, newGrid))
-		return 0;
+		return;
+	left = nleft;
+	right = nright;
+	bot = nbot;
 	clearGraphic();
-	memcpy(grid, newGrid, 4);
+	memcpy(grid, newGrid, 5);
 	draw();
-	return 1;
 }
 
-bool Block::move(byte direction)
+bool Block::move(int direction)
 {
 	if (direction == 0)
 		return 1;
-	byte dx, dy;
+	int dx, dy;
 	switch (direction)
 	{
 	case 1:		//right
@@ -420,9 +429,9 @@ bool Block::move(byte direction)
 		dy = 0;
 		break;
 	}
-	byte newX = position.x + dx;
-	byte newY = position.y + dy;
-	if (newX >= 0 && newX < 25 && newY >= 0 && newY < 56)
+	int newX = position.x + dx;
+	int newY = position.y + dy;
+	if (newX + left > 0 && newX + right < 27 && newY + bot < 57)
 	{
 		if (collision(Point(newX, newY), grid))
 			if (direction == 2)
@@ -434,5 +443,30 @@ bool Block::move(byte direction)
 		position.y = newY;
 		draw();
 	}
+	else
+		if (newY + bot == 57)
+			return 0;
 	return 1;
+}
+
+void Block::findBorder(int* left, int* right, int* bot, char grid[5])
+{
+	*bot = 4;
+	while (grid[*bot] == 0)
+		(*bot)--;
+	bool seeking = 1;
+	*left = 0;
+	while (!search(*left, grid))
+		(*left)++;
+	*right = 4;
+	while (!search(*right, grid))
+		(*right)--;
+}
+
+bool search(int x, char grid[5])
+{
+	for (int y = 0; y < 5; y++)
+		if (grid[y] & (1 << x))
+			return 1;
+	return 0;
 }
--- Tetris/main.h
+++ Tetris/main.h
@@ -3,12 +3,10 @@
 #include <utility>
 #include <unordered_set>
 
-typedef unsigned char byte;
-
 struct Point
 {
-	byte x, y;
-	Point(byte x0, byte y0);
+	int x, y;
+	Point(int x0, int y0);
 	Point(const Point& other);
 	Point() {}
 };
@@ -17,23 +15,27 @@ class Block
 {
 private:
 	Point position;
-	char grid[4] = { 0,0,0,0 };
+	char grid[5] = { 0,0,0,0,0 };
+	int left, right, bot;
+	void findBorder(int* left, int* right, int* bot, char grid[5]);
 public:
-	byte type;
+	int type;
 	sf::Color foreColor;
 	Block() {}
-	void set(sf::Color fColor, byte type, const Point& position);
-	bool rotate();
+	void set(sf::Color fColor, int type, const Point& position);
+	void rotate();
 	void projection();
 	void draw();
 	void clearGraphic();
-	bool collision(Point position, char datagrid[4]);
-	bool move(byte direction);
+	bool collision(Point position, char datagrid[5]);
+	bool move(int direction);	
 };
 
 void procKeyPress(const sf::Event& event);
 void procKeyUp(const sf::Event& event);
 void eliminate();
 void gameover();
-sf::RectangleShape* drawSquare(byte x, byte y, sf::Color color);
+sf::RectangleShape* drawSquare(int x, int y, sf::Color color);
 void init();
+bool search(int x, char grid[5]);
+
